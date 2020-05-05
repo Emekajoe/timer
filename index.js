0bf3fd84d83e1d6a72fc85bc2929e6a77a62d62e
Emeka <emekajoe5@gmail.com>
2020-05-05 04:26:27 +0100

    Added my project

---
+++ index.js
@@ -0,0 +1,33 @@
+//jshint esversion:9
+
+const durationInput = document.querySelector("#duration");
+const startButton = document.querySelector("#start");
+const pauseButton = document.querySelector("#pause");
+const circle = document.querySelector("circle");
+
+const perimeter = circle.getAttribute("r") * 2 * Math.PI;
+circle.setAttribute("stroke-dasharray", perimeter);
+
+let duration;
+let container=  document.querySelector('#container');
+
+function showAppreciation() {
+  container.style.visibility = 'visible';
+}
+
+const timer = new Timer(durationInput, startButton, pauseButton, {
+  onStart(totalDuration) {
+    duration = totalDuration;
+  },
+  onTick(timeRemaining) {
+    circle.setAttribute(
+      "stroke-dashoffset",
+      (perimeter * timeRemaining) / duration - perimeter
+    );
+  },
+  onComplete() {
+    console.log("Timer is completed");
+    showAppreciation();
+  },
+});
+// timer.start();
