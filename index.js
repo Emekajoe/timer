f02e12577cec9e0a573cb5bbf5e58c4751ebbeaa
Emeka <emekajoe5@gmail.com>
2020-05-05 04:41:23 +0100

    Added my project

--- index.js
+++ index.js
@@ -14,16 +14,21 @@ let container=  document.querySelector('#container');
 function showAppreciation() {
   container.style.visibility = 'visible';
 }
+function hideAppreciation() {
+  container.style.visibility = 'hidden';
+}
 
 const timer = new Timer(durationInput, startButton, pauseButton, {
   onStart(totalDuration) {
     duration = totalDuration;
+    hideAppreciation();
   },
   onTick(timeRemaining) {
     circle.setAttribute(
       "stroke-dashoffset",
       (perimeter * timeRemaining) / duration - perimeter
     );
+    hideAppreciation();
   },
   onComplete() {
     console.log("Timer is completed");
